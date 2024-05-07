fc3ac33fb6f089b3cb8bbb9568d3b6e2deabe180
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-05-07 19:31:44 +0100

    put and get

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,3 +12,17 @@ class BasicCache(BaseCaching):
     def __init__(self):
         '''initializer'''
         super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
